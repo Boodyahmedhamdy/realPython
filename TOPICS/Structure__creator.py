20d8c15c1ca55b2552f793e0fff3a051e34a7b36
unknown <abdelrahmanahmedhamdy99@gmail.com>
2021-10-03 11:09:03 +0200

    modified code from alot of functions to for loop

--- TOPICS/Structure__creator.py
+++ TOPICS/Structure__creator.py
@@ -6,18 +6,21 @@ init(autoreset=True)
 def make_green(thing):
     return f'{Fore.GREEN + str(thing) + Fore.RESET}'
 
+
 def createStructure():
 
     # take name of the folder
     nameOfParent = input("what is the name of TOPIC : ").upper().strip()
 
     import os
-    os.makedirs(f"{nameOfParent}/code")
-    os.makedirs(f"{nameOfParent}/resources")
-    os.makedirs(f"{nameOfParent}/text__files")
-    os.makedirs(f"{nameOfParent}/PROJECTS")
+    listOfFolders = ["code", 'resources', 'text__files', "project"]
+
+    for folder in listOfFolders:
+        os.makedirs(f'{nameOfParent}/{folder}')
+        open(f'{nameOfParent}/{folder}/.gitkeep', 'a')
 
     print(make_green("structure was made successfully ❤"))
+
 
 createStructure()
 
