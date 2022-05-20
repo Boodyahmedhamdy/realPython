eb1d9e24cd37e1ed330ab201482722d247c3553b
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-05-20 10:52:43 +0200

    added auto commiting

--- TOPICS/Structure__creator.py
+++ TOPICS/Structure__creator.py
@@ -1,4 +1,5 @@
 from colorama import Fore, init
+import subprocess
 
 init(autoreset=True)
 
@@ -22,11 +23,37 @@ def createStructure():
         os.makedirs(f'{nameOfParent}/{folder}')
 
         # create .gitkeep file to save the structure for the repo
-        open(f'{nameOfParent}/{folder}/.gitkeep', 'a')
+        with open(f'{nameOfParent}/{folder}/.gitkeep', 'a') as gitkeepFile:
+
+            # handling add to staging area first
+            gitkeepAddProcess = subprocess.run(["git", "add", f'{gitkeepFile}'],
+                                               capture_output=True)
+            # if something went wrong
+            if gitkeepAddProcess.returncode != 0:
+                print(f"something went wrong while adding {gitkeepFile}")
+
+            # handling commit with saved message
+            gitkeepCommitProcess = subprocess.run(["git", "commit", "-m", "'folder keeper'"],
+                                                  capture_output=True)
+
+            # if something went wrong
+            if gitkeepCommitProcess.returncode != 0:
+                print(f"something went wrong while commiting {gitkeepFile}")
 
         # create readme file
         with open(f'{nameOfParent}/{folder}/README.md', 'a') as readmeFile:
             readmeFile.write(f"# {folder} folder")
+
+            readmeAddProcess = subprocess.run(["git", "add", f'{readmeFile}'],
+                                              capture_output=True)
+            if readmeAddProcess.returncode != 0:
+                print(f"something went wrong while adding {readmeFile}")
+
+            readmeCommitProcess = subprocess.run(["git", "commit", "-m", "'README File'"],
+                                                 capture_output=True)
+
+            if readmeCommitProcess.returncode != 0:
+                print(f"something went wrong while commit {readmeFile}")
 
     print(make_green("structure was made successfully ❤"))
 
